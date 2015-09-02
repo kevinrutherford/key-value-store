d803f8a23b4b4f9e3a5d8e2370cbe128431f5142
Kevin Rutherford <kevin@rutherford-software.com>
2015-09-02 19:42:50 +0100

    Store fields

--- server.rb
+++ server.rb
@@ -1,7 +1,24 @@
 require 'sinatra/base'
+require 'json'
 
 class KeyValueStore < Sinatra::Application
-  get '/' do
-    redirect '/index.html'
+
+  configure do
+    set :pairs, {}
   end
+
+  get '/forms/:formid' do
+    content_type :json
+    return JSON.pretty_generate({
+      fields: settings.pairs
+    })
+  end
+
+  post '/forms/:formid/fieldvalue' do
+    request.body.rewind
+    payload = JSON.parse(request.body.read, symbolize_names: true)
+    settings.pairs[payload[:fieldName]] = payload[:fieldValue]
+    return 200
+  end
+
 end
